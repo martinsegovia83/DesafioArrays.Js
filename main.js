d6ac16909d2c6243c4e17032544cab66e8d59822
lucas segovia <martinsegovia83@hotmail.com>
2022-04-15 22:51:05 -0300

    xx

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ let numero = parseInt(prompt("Ingrese un numero"));
 
 for (let i = 1; i <= 10; i++){
     let multiplo = numero * i;
-    alert (numero + "X" + i + "=" + multiplo);
     console.log (numero + "X" + i + "=" + multiplo);
 }
