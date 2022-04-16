87bf8817f3a99ca4048a247c88e14fd630e06019
lucas segovia <martinsegovia83@hotmail.com>
2022-04-15 22:48:36 -0300

    primer ejercicio en Js

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let numero = parseInt(prompt("Ingrese un numero"));
+
+for (let i = 1; i <= 10; i++){
+    let multiplo = numero * i;
+    alert (numero + "X" + i + "=" + multiplo);
+    console.log (numero + "X" + i + "=" + multiplo);
+}
